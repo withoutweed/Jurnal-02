26b2678f784c6d6d4cb08cffebed05a688afe550
withoutweed <jingstro13@gmail.com>
2020-09-16 10:35:48 +0700

    Tugas.java

---
+++ Tugas.java
@@ -0,0 +1,8 @@
+public class Tugas {
+
+	public static void main (String[] args) {
+		System.out.println("Imam Yabunaiya");
+		System.out.println("6706204094");
+		System.out.println("D3IF-44-02");
+	}
+}
